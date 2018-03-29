224a31bbeb4258ae1b1ce0d0b6de2851b28f095c
Jack <jackexyou@gmail.com>
2018-03-28 23:20:32 -0400

    created rooms

--- app.js
+++ app.js
@@ -11,9 +11,19 @@ serv.listen(2000);
 console.log("server started");
 
 
-var io = require('socket.io')(serv,());
+var room = 0 
+
+var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket) {
 	console.log('socket connection');
 
+	socket.on('createGame', function(data){
+      socket.join('Room ' + ++room);
+      console.log('room ' + room + ' created')
+  });
+
+	socket.on('happy', function(data){
+		console.log("happy because" + data.reason)
+	})
 
 });
